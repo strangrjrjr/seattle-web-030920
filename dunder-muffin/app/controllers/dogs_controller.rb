9d997e9f3fc304718cb3d0fab462987d75be505c
Roger Robertson Jr <rjrjr@pm.me>
2020-04-08 17:12:52 -0700

    completed without bonus

--- dunder-muffin/app/controllers/dogs_controller.rb
+++ dunder-muffin/app/controllers/dogs_controller.rb
@@ -1,2 +1,47 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        set_dog
+       @employees = Employee.all.select {|e| e.dog_id == @dog.id}
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.new(dog_params)
+        if @dog.save
+            redirect_to @dog
+        else
+            render :new
+        end
+    end
+
+    def edit
+        set_dog
+    end
+
+    def update
+
+    end
+
+    def delete
+
+    end
+
+
+    private
+
+    def set_dog
+        @dog = Dog.find(params[:id])
+    end
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
 end
--- dunder-muffin/app/controllers/employees_controller.rb
+++ dunder-muffin/app/controllers/employees_controller.rb
@@ -1,2 +1,53 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+       set_employee
+       @dog = Dog.find(@employee.dog_id)
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to @employee
+        else
+            flash[:error_message] = @employee.errors.full_messages
+            render :new
+        end
+    end
+
+    def edit
+        set_employee
+    end
+
+    def update
+        set_employee
+        if @employee.update(employee_params)
+            redirect_to @employee
+        else
+            flash[:error_message] = @employee.errors.full_messages
+            render :edit
+        end
+    end
+
+    def delete
+
+    end
+
+
+    private
+
+    def set_employee
+        @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
 end
